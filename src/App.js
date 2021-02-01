99458cdaa730c022f8802833e8c65015d60d668b
RAVI <ravi13eng@gmail.com>
2021-02-01 14:18:10 +0530

    manucomponent added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponents';
 import './App.css';
+import { DISHES } from './shared/dishes';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes : DISHES
+    };
+  }
+
+  
   render(){
     return(
       <div className="App">
@@ -12,7 +23,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante con fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes = {this.state.dishes} />
       </div>
     );
   }
